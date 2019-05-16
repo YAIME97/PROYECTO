2b53bf8ab31c2af813a2fe8c7640ed17eae6a7e0
PABLO YAIME <jpyaimen@corhuila.edu.co>
2019-05-15 21:12:13 -0500

    Iniciamos versionamiento proyecto final2

---
+++ src/conexionBD/conectarBD.java
@@ -0,0 +1,13 @@
+package conexionBD;
+
+/**
+ *
+ * @author JUAN YAIME
+ */
+public class conectarBD {
+    public static void main(String[] args) {
+       // TODO code application logic here
+       conexion con = new conexion();
+       con.conectar();
+   }
+}
